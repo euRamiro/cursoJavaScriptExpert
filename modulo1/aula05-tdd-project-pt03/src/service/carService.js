e039f637ad79a95feae90105d5e3853621b337c1
euRamiro <euramiro@gmail.com>
2020-11-16 15:24:32 -0400

    Projeto final - TDD & BDD - parte 3

---
+++ modulo1/aula05-tdd-project-pt03/src/service/carService.js
@@ -0,0 +1,58 @@
+const BaseRespository = require('./../repository/base/baseRepository')
+const Tax = require('./../entities/tax')
+const Transaction = require('./../entities/transaction')
+
+class CarService {
+  constructor({cars}) {
+    this.carRepository = new BaseRespository({file: cars})
+    this.taxesBaseOnAge = Tax.taxesBaseOnAge
+    this.currencyFormat = new Intl.NumberFormat('pt-br', {
+      style: 'currency',
+      currency: 'BRL'
+    })
+  }
+
+  getRandomPositionFromArray(list){
+    const listLength = list.length
+    return Math.floor(
+      Math.random() * (listLength)
+    )
+  }
+
+  chooseRandomCar(carCategory){
+    const randomCarIndex = this.getRandomPositionFromArray(carCategory.carIds)
+    const carId = carCategory.carIds[randomCarIndex]
+    return carId
+  }
+
+  async getAvailableCar(carCategory){
+    const carId = this.chooseRandomCar(carCategory)
+    const car = await this.carRepository.find(carId)
+    return car
+  }
+
+  calculateFinalPrice(custumer, carCategory, numberOfDays){
+    const { age } = custumer
+    const { price} = carCategory
+    const { then: tax } = this.taxesBaseOnAge.find(tax => age >= tax.from && age <= tax.to)
+
+    const finalPrice = ((tax * price) * (numberOfDays))
+    const formattedPrice = this.currencyFormat.format(finalPrice)
+    return formattedPrice
+  }
+
+  async rent(customer, carCategory, numberOfDays){
+    const car = await this.getAvailableCar(carCategory)
+    const finalPrice = await this.calculateFinalPrice(customer, carCategory, numberOfDays)
+    const today = new Date()
+    today.setDate(today.getDate() + numberOfDays)
+    const options = { year: 'numeric', month: 'long', day: 'numeric'}
+    const dueDate = today.toLocaleDateString('pt-br',options)
+    const transaction = new Transaction({
+      customer, dueDate, car, amount: finalPrice
+    })
+    return transaction
+  }
+}
+
+module.exports = CarService
